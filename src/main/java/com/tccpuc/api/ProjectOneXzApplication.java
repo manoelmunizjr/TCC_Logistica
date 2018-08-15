f0b6d79b37dac8bdebd686d3ca4d385ae73d2242
Paula Muniz <paula.xavier@gmail.com>
2018-08-15 17:18:06 -0300

    Versão inicial do projeto

---
+++ src/main/java/com/tccpuc/api/ProjectOneXzApplication.java
@@ -0,0 +1,13 @@
+package com.tccpuc.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectOneXzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectOneXzApplication.class, args);
+		System.out.println("Running test my first API Spring Boot");
+	}
+}
